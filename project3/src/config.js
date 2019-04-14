29499c545b31cb701e2576a86585df4ca406c7b3
Cait Sidener <cait.sidener@gmail.com>
2019-04-13 20:27:03 -0500

    log in stuffs

---
+++ project3/src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_hPoOGEr2m",
+      APP_CLIENT_ID: "2c7kfderc028kc64u2gv8du4qa",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+  };
+  
