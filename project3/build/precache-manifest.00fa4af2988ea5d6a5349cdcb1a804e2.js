f855fd1dfc58dbc29288aafbfcb02f4dd59b47be
Cait Sidener <cait.sidener@gmail.com>
2019-04-18 20:25:26 -0500

    tabs and things

---
+++ project3/build/precache-manifest.00fa4af2988ea5d6a5349cdcb1a804e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "971ff2c68cfd699edaeb",
+    "url": "/static/js/main.ababc060.chunk.js"
+  },
+  {
+    "revision": "2c09fef459eafbf22eb5",
+    "url": "/static/js/2.ccbb6c31.chunk.js"
+  },
+  {
+    "revision": "971ff2c68cfd699edaeb",
+    "url": "/static/css/main.6a2307c5.chunk.css"
+  },
+  {
+    "revision": "c225480bf10e7a7c2d832c9ace7e5ef2",
+    "url": "/index.html"
+  }
+];
---
+++ project3/client/src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './App.css';
+import firebase, { auth, provider } from './firebase.js';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/Nav";
+import Dashboard from "./containers/Dashboard";
+import Discover from "./containers/Discover";
+import Saved from "./containers/Saved";
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+  
+  logout() {
+    auth.signOut()
+    .then(() => {
+      this.setState({
+        user: null
+      });
+    });
+  }
+  login() {
+    auth.signInWithPopup(provider)
+    .then((result) => {
+      const user = result.user;
+      this.setState({
+        user
+      });
+    });
+  }
+  
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ user });
+    }
+  });
+    
+  }
+  
+
+
+  render() {
+    return (
+      <div className='app'>
+      <header>
+        <div className="wrapper">
+          <h1>I Play Music</h1>
+          {this.state.user ?
+            <button onClick={this.logout}>Logout</button>                
+            :
+            <button onClick={this.login}>Log In</button>              
+          }
+        </div>
+      </header>
+      {this.state.user ?
+        <div>
+          <div className='user-profile'>
+            <img src={this.state.user.photoURL} />
+          </div>
+        
+          <Router>
+            <div>
+            <NavTabs />
+              <Route exact path="/dashboard" render={(props) => <Dashboard user={this.state.user} {...props} />} />
+              <Route exact path="/discover" component={Discover} />
+              <Route exact path="/saved" component={Saved} />
+            </div>
+          </Router>
+          </div>
+        :
+        <div className='wrapper'>
+          <p>Welcome to our app. This is our about page that explains what this app is all about.
+            Log in to head over to your dashboard!
+          </p>
+        </div>
+      }
+    </div>
+    )
+  }
+}
+
+export default App;
