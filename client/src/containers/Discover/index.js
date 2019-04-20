e52bcb10e11336d7e45389db378a2ba81c99af50
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-04-19 20:51:58 -0500

    api 2

--- client/src/containers/Discover/index.js
+++ client/src/containers/Discover/index.js
@@ -1,28 +1,51 @@
 import React from "react";
-import { retrieve } from "../../utils/API";
+import firebase from "../../firebase";
 
 class Discover extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            savedUsers: []
+            people: []
         }
     }
 
     componentDidMount() {
-        retrieve();
+        firebase.database().ref('items')
+            .on('value', (snapshot) => {
+                let items = snapshot.val();
+                let newState = [];
+                for (let item in items) {
+                    newState.push({
+                        id: item,
+                        title: items[item].title,
+                        user: items[item].user
+                    });
+                }
+                this.setState({
+                    people: newState
+                })
+            })
     }
 
     render() {
-        console.log(this.state.savedUsers);
-        return(
+        console.log(this.state.people)
+        return (
             <main>
-                <h1>Hi</h1>
+                <ul>
+                    {this.state.people.map((person) => {
+                        return (
+                            <li key={person.id}>
+                                <h3>{person.title}</h3>
+                                <h3>{person.user}</h3>
+                            </li>
+                )
+                    })}
+            </ul>
             </main>
-        );
-    }
-}
-
-export default Discover;
-
-
+                );
+            }
+        }
+        
+        export default Discover;
+        
+        
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,17 +1,19 @@
+import firebase from  "../firebase";
 // whole thing inside GET /api/items
 
 export const retrieve = () => {
     console.log("working")
-//     firebase.database().ref('items');
-//     itemsRef.on('value', (snapshot) => {
-//         let items = snapshot.val();
-//         let newState = [];
-//         for (let item in items) {
-//             newState.push({
-//                 id: item,
-//                 title: items[item].title,
-//                 user: items[item].user
-//             });
-//         }
-//     })
+    firebase.database().ref('items')
+    .on('value', (snapshot) => {
+        let items = snapshot.val();
+        let newState = [];
+        for (let item in items) {
+            newState.push({
+                id: item,
+                title: items[item].title,
+                user: items[item].user
+            });
+        }
+        console.log(newState)
+    })
 }
