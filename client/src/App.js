f0db8e0c885aa08a9f758a6047afb779f924e3de
Cait Sidener <cait.sidener@gmail.com>
2019-04-27 13:15:07 -0500

    stuff and things

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,7 @@ import Home from "./containers/Home";
 class App extends Component {
 
   render() {
-    return (
-      
-      <div>
-      <Home></Home>
-        </div>
-    )
+    return <Home />
   }
 }
 
